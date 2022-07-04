01c189825ad2bf13c482c9b373d2943845b28b38
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-04 10:49:21 -0700

    React App - Basic with Simple Project

---
+++ react-from-basic/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello Mangala Lets Jump in to the Basics via Simple Project </h1>
+      
+    </div>
+    
+  );
+}
+
+export default App;
