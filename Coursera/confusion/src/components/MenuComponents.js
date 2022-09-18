c9ad99a16e0641ef3cef66514158ebe2a8bf21ab
mangala-karunarathne <mangalaicc@gmail.com>
2022-09-18 16:50:49 -0700

    Components with Comments

--- Coursera/confusion/src/components/MenuComponents.js
+++ Coursera/confusion/src/components/MenuComponents.js
@@ -1,15 +1,17 @@
 import { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import DishdetailsComponent from './DishdetailsComponent';
 class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedDish: null
         }
-    }
+    };
+  
     onDishSelect(dish) {
         this.setState({ selectedDish: dish});
+        console.log(this.state.selectedDish)
     }
     renderDish(dish) {
         if (dish != null)
@@ -19,8 +21,41 @@ class Menu extends Component {
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
+                      <Button>
+                        Order
+                      </Button>
                     </CardBody>
+                    <CardText class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small>Donec id elit non mi porta.</small>
+                    </a>
+                        <div>
+                            console.log('Comments')
+                        </div>
+                    {/* <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small class="text-muted">Donec id elit non mi porta.</small>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                        </div>
+                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                        <small class="text-muted">Donec id elit non mi porta.</small>
+                    </a> */}
+                    </CardText>
                 </Card>
+                
             );
         else
             return(
@@ -35,12 +70,13 @@ class Menu extends Component {
                   onClick={() => this.onDishSelect(dish)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
+                      <CardTitle bold>{dish.name}</CardTitle>
                   </CardImgOverlay>
                 </Card>
               </div>
             );
         });
+        // console.log('Menu componenet render is invoked')
         return (
             <div className="container">
                 <div className="row">
@@ -50,6 +86,21 @@ class Menu extends Component {
                   <div  className="col-12 col-md-5 m-1">
                     {this.renderDish(this.state.selectedDish)}
                   </div>
+                </div>
+                <div className="row">
+                  <div  className="col-5 col-md-5 m-1">
+                    <DishdetailsComponent selectedDish={this.state.selectedDish}/>
+                  </div>
+                  <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">ABCDEF</p>
+                        <small>Donec id elit non mi porta.</small>
+                        <div>
+                            console.log('Comments')
+                        </div>
+                    
                 </div>
             </div>
         );
